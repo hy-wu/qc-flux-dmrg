627c46a81a17c6607ee4ddd8e6cc172d6ad3509b
吴浩源 <hy-wu20@mails.tsinghua.edu.cn>
2023-01-15 20:46:41 +0800

    replace Sub

--- flux.py
+++ flux.py
@@ -1,8 +1,8 @@
 import numpy as np
 from numpy import linalg as la
-from scipy import constants, sparse
-import Sub180221 as Sub
+from scipy import constants, sparse, linalg
 import time
+import Sub180221 as Sub
 
 
 # notations in comment
@@ -47,10 +47,12 @@ class Flux:
         phi_ext = 0
         self.L_bond = np.array([1, 0, 0.5 * self.E_Jb, 0.5 * self.E_Jb]).reshape((1, 4, 1))
         self.R_bond = np.array([0, 1, np.exp(1j * phi_ext), np.exp(-1j * phi_ext)]).reshape((1, 4, 1))
-        self.HL = [None for _ in range(N_J)]
+        self.HL = [self.L_bond for _ in range(N_J)]
+        # self.HL = [None for _ in range(N_J)]
         self.HL[0] = self.L_bond
-        self.HR = [None for _ in range(N_J)]
-        self.HR[-1] = self.R_bond
+        self.HR = [self.R_bond for _ in range(N_J)]
+        # self.HR = [None for _ in range(N_J)]
+        # self.HR[-1] = self.R_bond
         self.T = []
         self.Eng = []
         self.init_MPS()
@@ -67,9 +69,30 @@ class Flux:
             else:
                 self.T.append(np.random.rand(Dl, self.scale_, Dr, self.N_excited))
 
+        '''def Mps_LQP(TL, UR):
+            A = np.tensordot(TL, UR, (-1, 0))
+            A = A.reshape((A.shape[0], -1))
+            UL, TR = np.linalg.qr(A)
+            TR = TR.reshape(TL.shape)
+            return UL, TR'''
+            
+        def Mps_LQP(T,UR):  # TODO
+            """ (0-T-L)(0-UR-1) -> (0-UL-1)(0-Tnew-L) """
+            shapeT = np.asarray(np.shape(T))
+            rankT = len(shapeT)
+            
+            A = np.tensordot(T,UR,(rankT-1,0))
+            A = np.reshape(A,[shapeT[0],np.prod(shapeT[1:])])
+            UL,Tnew = linalg.rq(A,mode = 'economic')
+            Sign = np.diag(np.sign(np.diag(UL)))
+            UL = np.dot(UL,Sign)
+            Tnew = np.dot(Sign,Tnew)
+            Tnew = np.reshape(Tnew,shapeT)
+            return UL, Tnew
+
         U = np.eye(np.shape(self.T[-1])[-1])
         for i in range(self.N_J - 1, 0, -1):
-            U, self.T[i] = Sub.Mps_LQP(self.T[i], U)
+            U, self.T[i] = Mps_LQP(self.T[i], U)
 
     def set_MPO(self, basis: str):
         if basis == 'charge':
@@ -108,22 +131,33 @@ class Flux:
         self.HR[-1] = self.R_bond
         for i in range(self.N_J - 1, 0, -1):
             # # print(i)
-            self.HR[i - 1] = Sub.NCon([
+            '''self.HR[i - 1] = Sub.NCon([
                 self.HR[i], self.T[i], self.MPO, np.conj(self.T[i])
             ], [
                 [1, 3, 5], [-1, 2, 1], [-2, 2, 3, 4], [-3, 4, 5]
-            ])
+            ])'''
+            self.HR[i - 1] = np.einsum(
+                self.HR[i], [1, 3, 5],
+                self.T[i], [11, 2, 1],
+                self.MPO, [12, 2, 3, 4],
+                np.conj(self.T[i]), [13, 4, 5],
+                [11, 12, 13]
+            )
 
     def optimizeT_site(self, site_: int):
         g = self.N_excited  # as noted in reference
         if self.usingLanczos:   # using block Lanczos
-            HL, HR, T = self.HL[site_], self.HR[site_], self.T[site_]
+            HL = self.HL[site_]   # type: np.ndarray
+            HR = self.HR[site_]   # type: np.ndarray
+            T = self.T[site_]   # type: np.ndarray
             DT = np.shape(T)
             # H:
             # -1    -2   -3
             # HL 1 MPO 2 HR
             # -4    -5   -6
-            H = Sub.NCon([HL, self.MPO, HR], [[-1, 1, -4], [1, -5, 2, -2], [-6, 2, -3]])
+            '''H = Sub.NCon([HL, self.MPO, HR], [[-1, 1, -4], [1, -5, 2, -2], [-6, 2, -3]])'''
+            H = np.einsum(HL, [11, 1, 14], self.MPO, [1, 15, 2, 12], HR, [16, 2, 13], [11, 12, 13, 14, 15, 16])
+            
             Psi = [T for _ in range(self.N_Lanczos)]
             Alpha = [np.matrix(np.zeros((g, g)), dtype=complex)
                      for _ in range(self.N_Lanczos)]
@@ -136,7 +170,6 @@ class Flux:
                 # - Psi_n - = - U - ,  D  ,  V
                 #      \         \      \     \
                 U, D, Vh = la.svd(Psi[n].reshape((-1, g)), full_matrices=False)
-
                 # step B
                 #   |     \          |
                 # - U - ,  V   =  - Psi_n -
@@ -151,17 +184,17 @@ class Flux:
                 # [      |       ]
                 # [-   Psi[n]  - ]
                 #         \
-                HPsi = Sub.NCon([H, Psi[n]], [[-1, -2, -3, 1, 2, 3], [1, 2, 3, -4]])
-
+                HPsi = np.einsum(H, [11, 12, 13, 1, 2, 3], Psi[n], [1, 2, 3, 14], [11, 12, 13, 14])
                 # Alpha_n = <Psi_n|H|Psi_n>
-                Alpha[n] = np.matrix(Sub.NCon([HPsi, np.conj(Psi[n])], [[1, 2, 3, -2], [1, 2, 3, -1]]))
+                Alpha[n] = np.matrix(np.einsum(HPsi, [1, 2, 3, 12],
+                                               np.conj(Psi[n]), [1, 2, 3, 11],
+                                               [11, 12]))
                 M[n * g:g + n * g, n * g:g + n * g] = Alpha[n]
 
                 # |Psi_{n+1}> = H|Psi_n> - Alpha_n|Psi_n> - Alpha_n^H|Psi_{n-1}>
                 if n < self.N_Lanczos - 1:
                     # step D
                     Psi[n + 1] = HPsi - np.tensordot(Psi[n], Alpha[n], (-1, 0))
-
                     if n:
                         # Beta_n
                         Beta[n] = np.matrix(np.asmatrix(Vh).H @ np.diag(D) @ Vh)
@@ -179,16 +212,20 @@ class Flux:
 
             Eig, V = sparse.linalg.eigsh(M, k=g, which='SA')
             V = np.reshape(V, (self.N_Lanczos, g, g))
-            psi = Sub.NCon([V, np.array(Psi)], [[1, 2, -4], [1, -1, -2, -3, 2]])
+            '''psi = Sub.NCon([V, np.array(Psi)], [[1, 2, -4], [1, -1, -2, -3, 2]])'''
+            psi = np.einsum(V, [1, 2, 14], np.array(Psi), [1, 11, 12, 13, 2], [11, 12, 13, 14])
             return psi, Eig
         else:
             HL, HR, T = self.HL[site_], self.HR[site_], self.T[site_]
-            A = Sub.NCon([HL, self.MPO, HR], [[-1, 1, -4], [1, -5, 2, -2], [-6, 2, -3]])
+            A = np.einsum(HL, [11, 1, 14], self.MPO, [1, 15, 2, 12], HR, [16, 2, 13], [11, 12, 13, 14, 15, 16])
+            '''A = Sub.NCon([HL, self.MPO, HR], [[-1, 1, -4], [1, -5, 2, -2], [-6, 2, -3]])'''
             # A:
             # -1    -2   -3
             # HL 1 MPO 2 HR
             # -4    -5   -6
-            A = Sub.Group(A, [[0, 1, 2], [3, 4, 5]])
+            '''A = Sub.Group(A, [[0, 1, 2], [3, 4, 5]])'''
+            As = A.shape
+            A = A.reshape((As[0]*As[1]*As[2], As[3]*As[4]*As[5]))
             Eig, V = sparse.linalg.eigsh(A, k=self.N_excited, which='SA')
             return np.reshape(V, np.shape(T)), Eig
 
@@ -212,7 +249,7 @@ class Flux:
 
     def optimizeT(self, usingLanczos: bool, Eng_criterion: float, N_criterion: int, Quiet: bool):
         self.usingLanczos = usingLanczos
-        self.init_MPS()
+        # self.init_MPS()
         Eng0 = np.zeros((self.N_J, self.N_excited))
         Eng1 = np.zeros((self.N_J, self.N_excited))
 
@@ -221,11 +258,18 @@ class Flux:
                 Ti, Eng1[i] = self.optimizeT_site(i)  # time cost most: optimizeT_site
                 U, S, V = self.SVD_T(Ti)
                 self.T[i] = U
-                self.HL[i + 1] = Sub.NCon([
+                '''self.HL[i + 1] = Sub.NCon([
                     self.HL[i], np.conj(self.T[i]), self.MPO, self.T[i]
                 ], [
                     [1, 3, 5], [1, 2, -1], [3, 4, -2, 2], [5, 4, -3]
-                ])
+                ])'''
+                self.HL[i + 1] = np.einsum(
+                    self.HL[i], [1, 3, 5],
+                    self.T[i], [1, 2, 11],
+                    self.MPO, [3, 4, 12, 2],
+                    np.conj(self.T[i]), [5, 4, 13],
+                    [11, 12, 13]
+                )
                 #                   |3              |3
                 # 0- S - V -1 1- T[i+1] -4 -> 0- T[i+1] - 4
                 #         \2                           \2
@@ -243,11 +287,18 @@ class Flux:
                 Ti = np.einsum('ijkl->kjil', Ti)
                 U, S, V = self.SVD_T(Ti)
                 self.T[i] = np.einsum('ijk->kji', U)
-                self.HR[i - 1] = Sub.NCon([
+                '''self.HR[i - 1] = Sub.NCon([
                     self.HR[i], self.T[i], self.MPO, np.conj(self.T[i])
                 ], [
                     [1, 3, 5], [-1, 2, 1], [-2, 2, 3, 4], [-3, 4, 5]
-                ])
+                ])'''
+                self.HR[i - 1] = np.einsum(
+                    self.HR[i], [1, 3, 5],
+                    self.T[i], [11, 2, 1],
+                    self.MPO, [12, 2, 3, 4],
+                    np.conj(self.T[i]), [13, 4, 5],
+                    [11, 12, 13]
+                )
                 #       |1                          |1
                 # 0- T[i-1] -2 2- V - S -3 -> 0- T[i-1] - 3
                 #                  \4              \4
@@ -256,7 +307,7 @@ class Flux:
                                           [0, 1, 3, 4])
 
             if not Quiet:
-                print(np.round(Eng1[1][:N_criterion] - Eng0[1][:N_criterion], 3), end='\r')
+                print('\r' + str(np.round(Eng1[1][:N_criterion] - Eng0[1][:N_criterion], 3)), end='\r')
             if (abs(Eng1[1][:N_criterion] - Eng0[1][:N_criterion]) < Eng_criterion).all():
                 break
             Eng0 = Eng1.copy()
